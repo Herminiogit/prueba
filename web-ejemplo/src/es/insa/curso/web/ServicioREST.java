f150fe1c9bc187895549ac8a3f05813ae33e6111
Nazaret <nazaretxx@gmail.com>
2014-09-04 11:23:54 +0200

    comentario tonto sobre servicio REST

--- web-ejemplo/src/es/insa/curso/web/ServicioREST.java
+++ web-ejemplo/src/es/insa/curso/web/ServicioREST.java
@@ -5,7 +5,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
-
+// comentario para commit
 @Path("/")
 public class ServicioREST {
 
